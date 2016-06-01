8a25aeafcf0aa5374f4c0369ddb987a0fe7afe28
Aqui <Aqui@192.168.0.102>
2016-05-31 23:20:05 -0400

    Creacion del modulo empresa Se creo este primer modulo con la herramienta gii

---
+++ frontend/modules/empresa/Empresa.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace frontend\modules\empresa;
+
+/**
+ * empresa module definition class
+ */
+class Empresa extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'frontend\modules\empresa\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
